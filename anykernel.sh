91feca6c059da60156dc820762b580fde6bacb5b
Detrivia <108690954+KDEFFALT@users.noreply.github.com>
2025-02-22 07:55:48 +0700

    Update anykernel.sh
    change de device to support topaz sapphire and xun

--- anykernel.sh
+++ anykernel.sh
@@ -4,17 +4,17 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=ExampleKernel by osm0sis @ xda-developers
+kernel.string=by osm0sis @ xda-developers
 do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=maguro
-device.name2=toro
-device.name3=toroplus
-device.name4=tuna
-device.name5=
+device.name1=topaz
+device.name2=tapas
+device.name3=sapphire
+device.name4=sapphiren
+device.name5=xun
 supported.versions=
 supported.patchlevels=
 supported.vendorpatchlevels=
