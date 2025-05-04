6a2416149cc7d3a4f67a236d363efe39ae4c4145
env. <162432464+belowzeroiq@users.noreply.github.com>
2025-05-04 23:36:48 +0700

    anykernel: update kernel string and enhance kernel selection logic

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=by osm0sis @ xda-developers
+kernel.string=by belowzeroiq @ github
 do.devicecheck=1
 do.modules=0
 do.systemless=0
@@ -31,6 +31,66 @@ no_magisk_check=1
 
 # import functions/variables and setup patching - see for reference (DO NOT REMOVE)
 . tools/ak3-core.sh
+
+# Kernel selection function
+choose_kernel() {
+  ui_print " "
+  ui_print "Kernel Version Selection:"
+  ui_print " "
+  ui_print "  -EN"
+  ui_print "  Volume Up: non-KSU version"
+  ui_print "  Volume Down: KSU version"
+  ui_print " "
+  ui_print "  -ID"
+  ui_print "  Volume Atas: versi non-KSU"
+  ui_print "  Volume Bawah: versi KSU"
+  ui_print " "
+  ui_print "Input: "
+  ui_print " "
+  ui_print " "
+
+  while true; do
+    input=$(getevent -qlc 1 2>/dev/null | grep -E "KEY_VOLUME(UP|DOWN)")
+    case "$input" in
+      *KEY_VOLUMEUP*)
+        return 1
+        ;;
+      *KEY_VOLUMEDOWN*)
+        return 2
+        ;;
+    esac
+    sleep 0.1
+  done
+}
+
+# Handle kernel selection
+if [ -f "$AKHOME/Image.ksu" ] && [ -f "$AKHOME/Image.noksu" ]; then
+  choose_kernel
+  case $? in
+    1)
+      ui_print " "
+      ui_print "Selected: non-KSU Kernel"
+      mv -f "$AKHOME/Image.noksu" "$AKHOME/Image"
+      ;;
+    2)
+      ui_print " "
+      ui_print "Selected: KSU Kernel"
+      mv -f "$AKHOME/Image.ksu" "$AKHOME/Image"
+      ;;
+  esac
+elif [ -f "$AKHOME/Image" ]; then
+  ui_print " "
+  ui_print "Single image kernel found, flashing it"
+  mv -f "$AKHOME/Image.ksu" "$AKHOME/Image"
+elif [ -f "$AKHOME/Image.ksu" ]; then
+  ui_print " "
+  ui_print "Only KernelSU version found, flashing it"
+  mv -f "$AKHOME/Image.ksu" "$AKHOME/Image"
+elif [ -f "$AKHOME/Image.noksu" ]; then
+  ui_print " "
+  ui_print "Only Standard version found, flashing it"
+  mv -f "$AKHOME/Image.noksu" "$AKHOME/Image"
+fi
 
 # boot install
 if [ -L "/dev/block/bootdevice/by-name/init_boot_a" -o -L "/dev/block/by-name/init_boot_a" ]; then
